9e0d7d04acf5864e6a70cb5834d8609020e8dfd5
Jeremie <jeremie.bonal@gmail.com>
2016-02-02 10:42:02 +0100

    Reworked the index.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 require 'sinatra'
+require 'sinatra/reloader'
 
 techs = Nokogiri::HTML.parse(open("https://www.ruby-toolbox.com/categories/by_name", 'User-Agent' => 'firefox').read)
 techs = techs.css(".group_items .link").map(&:children).map(&:first).map(&:to_s).map(&:strip)
@@ -12,8 +13,16 @@ get '/:id' do
   "<ul>"+
   techs.sample(params[:id].to_i).map { |e| "<li> <a href='https://www.ruby-toolbox.com/categories/#{e.gsub(' ','_')}'>#{e}</a></li>"}.join('')+
   "</ul>"
+  #   @technos = techs.sample(params[:id].to_i)
+  # erb :technologies
 end
 
 get '/' do
-  "Use : /[number-of-technologies]"
+  @maxnumber = techs.size
+  erb :index
+  # "Use : /[number-of-technologies]"
+end
+
+post '/' do
+  redirect "/#{params[:technumber]}"
 end
